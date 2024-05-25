c346e5bf6f0160c2e2726c5b6a7e1ec225e66421
HamzaSourani <hamzasourani98@gmail.com>
2024-05-25 22:46:57 +0400

    add logout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import bg from "./assets/bg.png";
+import { Button } from "./components/ui/button";
 function App() {
   const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.clear();
+  };
   useEffect(() => {
     if (!!!localStorage.getItem("userInfo")) {
       navigate("/login");
@@ -13,11 +17,16 @@ function App() {
       style={{ backgroundImage: `url(${bg})` }}
       className="h-screen overflow-hidden bg-red-300 bg-blend-hard-light bg-contain  bg-center"
     >
-      <div className="max-w-[1200px] m-auto">
-        <header className="py-4 px-12">
-          <h1 className="  py-2 -skew-x-12  px-8 rounded-xl text-xl md:text-2xl  lg:text-3xl font-bold bg-white/50  backdrop-blur-sm ">
-            الحكواتية{" "}
-          </h1>
+      <div className=" m-auto">
+        <header className="flex shadow-lg justify-between items-center py-2   px-8    bg-white/50  backdrop-blur-sm  mb-4">
+          <Button onClick={handleLogout}>تسجيل خروج</Button>
+          <div className="   ">
+            <img
+              className="w-16 rounded-md aspect-square object-cover "
+              src="/assets/images/logo.png"
+              alt="al hakawati"
+            />
+          </div>
         </header>
         <Outlet />
       </div>
